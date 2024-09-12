6496565765d1ba046f6ef410eb3cc03b535b38f9
vKemo <kareemmaher2002@icloud.com>
2024-09-12 15:18:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = var.region
+  region     = "eu-north-1"
+  access_key = "AKIAS6OOBLBMB6NIZRII"
+  secret_key = "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"
 }
-
 resource "aws_s3_bucket" "test" {
   bucket = var.bucket_name
   acl    = "private"
