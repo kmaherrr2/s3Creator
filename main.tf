0a1f6252a294d8f173dba50119cb8f3c19026ec6
vKemo <kareemmaher2002@icloud.com>
2024-09-12 17:43:22 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
 provider "aws" {
   region = "eu-north-1"
 }
-
-variable "bucket_name" {
-  type        = string
-}
-
-variable "versioning" {
-  type        = bool
-}
-
 resource "aws_s3_bucket" "test" {
   bucket = var.bucket_name
   acl    = "private"
