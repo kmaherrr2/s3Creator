97da0c9424e2e7268f509fa908f00db1f245b47e
vKemo <kareemmaher2002@icloud.com>
2024-09-12 17:36:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
 provider "aws" {
-  region     = "eu-north-1"
+  region = "eu-north-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning" {
+  description = "Enable versioning on the S3 bucket"
+  type        = bool
 }
 
 resource "aws_s3_bucket" "test" {
   bucket = var.bucket_name
   acl    = "private"
+  
   versioning {
     enabled = var.versioning
   }
