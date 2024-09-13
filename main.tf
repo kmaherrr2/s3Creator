aff91f38c929a930c86588cd3148cd73c19356a2
vKemo <kareemmaher2002@icloud.com>
2024-09-13 17:05:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 provider "aws" {
   region = "eu-north-1"
 }
+
+resource "random_string" "bucketrandom" {
+  length  = 10  
+  upper   = true
+  lower   = true
+  number  = true
+  special = false
+}
+
+# Use the random string in the S3 bucket name
 resource "aws_s3_bucket" "test" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${random_string.bucketrandom.result}"
   acl    = "private"
   
   versioning {
