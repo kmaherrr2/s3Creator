56eb4e93c6db9909c09de8b91a1194680de05bb3
Kareem Maher <kareemmaher2002@icloud.com>
2025-02-17 19:15:35 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "random_string" "bucketrandom" {
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket = "${var.bucket_name}${random_string.bucketrandom.result}"
+  bucket = "${var.bucket_name}"
   acl    = "private"
   
   versioning {
